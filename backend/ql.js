774d8960daf7f2cea85b9fe2d09e005d96aa3366
PoemSoul <poemsoul@vip.qq.com>
2022-09-02 14:47:24 +0800

    适配青龙2.13.3

--- backend/ql.js
+++ backend/ql.js
@@ -9,7 +9,7 @@ const qlDataDir = process.env.QL_Data_DIR || '/ql/data';
 const authFile = path.join(qlDataDir, 'config/auth.json');
 
 const api = got.extend({
-  prefixUrl: process.env.QL_URL || 'http://localhost:5600',
+  prefixUrl: process.env.QL_URL || 'http://localhost:5700',
   retry: { limit: 0 },
 });
 
